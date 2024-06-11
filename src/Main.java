94d4c1a729d8824b71a7cf265cc573e23ee6ab0f
fdiasm <fernandodiasm@gmail.com>
2024-06-11 20:48:34 -0300

    Added class Mentoria and Curso

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descricao curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setDescricao("descricao curso JS");
+        curso2.setTitulo("curso JS");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria de Java");
+        mentoria1.setDescricao("descricao mentoria Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+}
